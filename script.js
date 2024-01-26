82b78d325d0726f7c4ad374eff910d031bbbef3e
kylekrause3 <kkrause317@gmail.com>
2024-01-25 23:06:26 -0500

    Added styles, social links

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Welcome to my website!");
+console.log("have you heard of the game?");
+
+function updateGreeting() {
+    const hour = new Date().getHours();
+    let greeting;
+
+
+    if (hour < 5) {
+        greeting = "Good Evening! (but it's technically morning)";
+    }
+    else if (hour < 12) {
+        greeting = "Good Morning!";
+    }
+    else if (hour < 18) {
+        greeting = "Good Afternoon!";
+    }
+    else {
+        greeting = "Good Evening!";
+    }
+
+    document.getElementById('greeting').textContent = greeting;
+}
+
+// Call the function when the page loads
+updateGreeting();
